436a80f9ff46a2478f2919900af83e67a5c5be69
Fawaz <fawazming@gmail.com>
2025-02-04 03:32:21 +0100

    improved

--- js/rayyantech.js
+++ js/rayyantech.js
@@ -13,31 +13,17 @@ function displayPosts(posts) {
     const postList = document.getElementById('galwrap');
     posts.forEach(post => {
         const listItem = document.createElement('div');
-        let newHTML = `<div class="col-lg-4 col-md-6 col-12 picture-item" data-groups='["branding"]'>
+        listItem.classList.add('col-lg-4', 'col-md-6', 'col-12', 'picture-item')
+        listItem.innerHTML = `
                     <div class="work-container position-relative d-block overflow-hidden rounded">
                         <div class="card-body p-0">
                             <img src="images/portfolio/01.jpg" class="img-fluid" alt="work-image">
-                            <div class="overlay-work bg-dark"></div>
-                            <div class="content">
-                                <h5 class="mb-3"><a class='content-title' href='/queue/layout/page-portfolio-detail'>Mockup Collection</a></h5>
-                                <h6 class="text-light tag mb-0">Mockup</h6>
+                            
+                            <div class="">
+                                <h5 class="mb-3" style="margin-left:5px"><a class="content-title" href="?postId=${post.id}">${post.title}</a></h5>
                             </div>
                         </div>
-                    </div>
-                </div>`;
-        listItem.innerHTML = `<a href="?postId=${post.id}">${post.title}</a>`;
-        listItem.innerHTML = ` <div class="col-lg-4 col-md-6 col-12 picture-item" data-groups='["branding"]'>
-                    <div class="work-container position-relative d-block overflow-hidden rounded">
-                        <div class="card-body p-0">
-                            <img src="images/portfolio/01.jpg" class="img-fluid" alt="work-image">
-                            <div class="overlay-work bg-dark"></div>
-                            <div class="content">
-                                <h5 class="mb-3"><a class="content-title" href="?postId=${post.id}">${post.title}</a></h5>
-                                <h6 class="text-light tag mb-0">Mockup</h6>
-                            </div>
-                        </div>
-                    </div>
-                </div>`;
+                    </div>`;
         postList.appendChild(listItem);
     });
 }
