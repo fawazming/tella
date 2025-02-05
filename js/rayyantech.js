e3466fb51583345c74a81c2ce60822b63df78918
Fawaz <fawazming@gmail.com>
2025-02-05 18:57:29 +0100

    blog

--- js/rayyantech.js
+++ js/rayyantech.js
@@ -1,5 +1,5 @@
 // Obfuscate your API key
-const API_KEY = 'AIzaSyBzYC473sbzrK4QQG3zcRp_kPl_moqvEdo'; // Replace with your actual API key
+const API_KEY = 'AIzaSyBzYC473sbzrK4QQG3zcRp_kPl_moqvEdo';
 
 // Function to fetch all posts
 async function fetchPosts(blogId) {
@@ -9,7 +9,7 @@ async function fetchPosts(blogId) {
 }
 
 // Function to display posts in a list
-function displayPosts(posts) {
+function displayPPosts(posts) {
     const postList = document.getElementById('galwrap');
     posts.forEach(post => {
         const listItem = document.createElement('div');
@@ -21,6 +21,31 @@ function displayPosts(posts) {
                             
                             <div class="">
                                 <h5 class="mb-3" style="margin-left:5px"><a class="content-title" href="?postId=${post.id}">${post.title}</a></h5>
+                            </div>
+                        </div>
+                    </div>`;
+        postList.appendChild(listItem);
+    });
+}
+
+// Function to display posts in a list
+function displayBPosts(posts) {
+    const postList = document.getElementById('blogwrap');
+    posts.forEach(post => {
+        const listItem = document.createElement('div');
+        listItem.classList.add('col-xxl-4', 'col-xl-4', 'col-lg-4', 'col-md-4')
+        listItem.innerHTML = `
+                    <div class="blog-box">
+                        <div class="blog-images">
+                            <img src="images/blog/blog-05.jpg" class="img-fluid rounded" alt="blog image">
+                        </div>
+                        <div class="blog-content">
+                            <h6 class="blog-title mt-4">
+                                <a href="#">${post.title}</a>
+                            </h6>
+
+                            <div class="read-link mt-4">
+                                <a class="text-uppercase text-dark" href="?postId=${post.id}">Read More</a>
                             </div>
                         </div>
                     </div>`;
@@ -47,20 +72,35 @@ function displayPost(post) {
 // Main function to handle the logic
 async function main() {
     console.log("MAin fun called")
-    const blogId = '3754457232408122297';
-    const urlParams = new URLSearchParams(window.location.search);
-    const postId = urlParams.get('postId');
+    const portfolioId = '3754457232408122297';
+    const BlogId = '3642558814134041603';
+    const urlParamsP = new URLSearchParams(window.location.search);
+    const urlParamsB = new URLSearchParams(window.location.search);
+    const singlePortfolioId = urlParamsP.get('singlePortfolioId');
+    const singleBlogId = urlParamsB.get('singleBlogId');
 
-    if (postId) {
-        const post = await fetchPost(blogId, postId);
+    if (singlePortfolioId) {
+        const post = await fetchPost(portfolioId, singlePortfolioId);
         displayPost(post);
     } else {
         console.log("MAin fun posts called")
-        const posts = await fetchPosts(blogId);
+        const posts = await fetchPosts(portfolioId);
         console.log(posts)
-        displayPosts(posts);
+        displayPPosts(posts);
+    }
+
+    if (singleBlogId) {
+        const post = await fetchPost(BlogId, singleBlogId);
+        displayPost(post);
+    } else {
+        console.log("MAin fun blogs called")
+        const posts = await fetchPosts(BlogId);
+        console.log(posts)
+        displayBPosts(posts);
     }
 }
+
+
 
 // Call the main function on page load
 window.onload = main;
